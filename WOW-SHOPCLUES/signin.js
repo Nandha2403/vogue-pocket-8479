ee663d6679eea034b6781d27a937c9b589118970
Nandha2403 <115460581+Nandha2403@users.noreply.github.com>
2023-01-21 07:59:05 +0530

    Day-3

---
+++ WOW-SHOPCLUES/signin.js
@@ -0,0 +1,24 @@
+let data=JSON.parse(localStorage.getItem("singindata"));
+let form=document.querySelector("form");
+form.addEventListener("submit",submit)
+    
+    function submit(event){
+        event.preventDefault();
+    let email=form.email.value;
+    let password=form.password.value;
+    console.log(email,password);
+    function submit(data){
+        data.forEach((el) => {
+            if(el.email==email && el.password==password){
+                alert("Login Success")
+                window.location.href="./index.html"
+                
+            }else{
+                alert("Wrong Credentials")
+                window.location.href="./signin.html"
+            }
+        });
+    }
+    submit(data)
+    }
+    
